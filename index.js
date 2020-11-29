2cface43347f40f5ff23ca1ede166296d6da9567
jpeterson14 <jpeterson0514@gmail.com>
2020-11-29 16:28:16 -0600

    commit tree 28f3d5ffcda12c02032866123eb83f5adbc66e77

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+const mapToNegativize = (sourceArray) => {
+    let negativeArray = []
+    sourceArray.forEach( num => {
+    let x = num * -1
+    negativeArray.push(x)
+    })
+    return negativeArray
+}
+const mapToNoChange = (sourceArray) => {
+    let sameArray = []
+    sourceArray.forEach( num => {
+    let x = num 
+    sameArray.push(x)
+    })
+    return sameArray
+}
+const mapToDouble = (sourceArray) => {
+    let doubledArray = []
+    sourceArray.forEach( num => {
+    let x = num * 2
+    doubledArray.push(x)
+    })
+    return doubledArray
+}
+const mapToSquare = (sourceArray) => {
+    let squaredArray = []
+    sourceArray.forEach( num => {
+    let x = num * num
+    squaredArray.push(x)
+    })
+    return squaredArray
+}
+
+
